955d92a3c43f9dae9d7b7e284d458d9a8f59ba2c
unknown <you@example.com>
2023-03-13 20:14:58 -0300

    clasemitad

--- objetos/objectprototype.js
+++ objetos/objectprototype.js
@@ -1,36 +1,44 @@
-console.log("OBJECT BUH", Object);
-      //console.log("OBJECT PONELE", Object.getPrototypeOf({}));
-      // Native code is program source code that has been compiled to native machine code.
-      // console.log("GLOBAL OBJECT => OBVIO ES UN WHAT???", typeof Object);
+//console.log("OBJECT BUH", Object);
+//console.log("%cNative code is program source code that has been compiled to native machine code.","color: white; background-color: black; font-size: 40px");
+//console.log("OBJECT PONELE", Object.getPrototypeOf({pepe: "pepe2"}));
+//console.log("GLOBAL OBJECT => OBVIO ES UN WHAT???", typeof Object);
+//console.log(typeof 4)
+//!
 
-      /////////////////////////////////////////////////////////////////////////////
+/* console.log("OBJECT PONELE", console);
+console.error("OHHH NO ENTIENDO NADA") */
 
-/*       let dia = new Date();
-      console.log("OBJECT DATE", Object.getPrototypeOf(dia));
+//!
 
-      /////////////////////////////////////////////////////////////////////////////
-
-      let array = [1];
+   /*    let dia = new Date();
+      console.log("OBJECT DATE", Object.getPrototypeOf(dia)); */
+/*
+      //!
+      let array = [1,2,3];
       console.log("ARRAY", Object.getPrototypeOf(array));
 
-      /////////////////////////////////////////////////////////////////////////////
-
-      let cadena = "pepe";
+      //!
+*/
+     /*  let cadena = "pepe";
       console.log("STRING", Object.getPrototypeOf(cadena));
-
-      /////////////////////////////////////////////////////////////////////////////
+      
+      console.log(cadena.replaceAll("p","x"))
+      //!
 
       let numero = 1;
       console.log("NUMBER", Object.getPrototypeOf(numero));
 
-      /////////////////////////////////////////////////////////////////////////////
+      //!
 
       let booleano = false;
-      console.log("BOOLEANO", Object.getPrototypeOf(booleano));    
+      console.log("BOOLEANO", Object.getPrototypeOf(booleano));     */
 
-      /////////////////////////////////////////////////////////////////////////////
+      //!
 
-      const pedro = () => null;
-      console.log("FUNCION", Object.getPrototypeOf(pedro));
-      // Native code is program source code that has been compiled to native machine code.
-      console.log(typeof pedro); */
+      const pedro = () => "white";
+
+
+     console.log("FUNCION", Object.getPrototypeOf(pedro));
+      console.log(2, typeof pedro); 
+      
+
